087bcc5052316bdf817159715190ec1969fd9b3c
BashaVali <bashavali222@gmail.com>
2023-02-19 04:30:08 +0530

    [Basha]Add:SnakeAndLadderProblem

---
+++ SnakeAndLadderProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SnakeAndLadderSimulator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the snake and ladder game");
+            Console.WriteLine("---------------------------------------");
+            Game game = new Game();
+            game.GameStart();
+            Console.ReadLine();
+        }
+    }
+}
